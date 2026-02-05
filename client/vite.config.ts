8cd680b939aadcfcb15e2346693db13de9c17c7e
thedunerats <thedunerats@yahoo.com>
2026-02-05 11:41:53 -0800

    Update vite.config.ts

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -21,11 +21,5 @@ export default defineConfig({
     globals: true,
     environment: 'jsdom',
     setupFiles: './src/setupTests.ts',
-    pool: 'forks',
-    poolOptions: {
-      forks: {
-        singleFork: true,
-      },
-    },
   },
 })
